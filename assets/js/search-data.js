3f5990e11683002da33a67d2603323ac3d3289af
scoczy <151121329+scoczy@users.noreply.github.com>
2026-05-13 07:32:31 +0000

    Deploying to gh-pages from @ scoczy/scoczy.github.io@0a22b3a0944d167a713503d9bddadb9cef2a8bc6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,7 +420,12 @@ ninja.data = [{
           section: "News",},{id: "news-prepared-a-research-manuscript-on-passive-tilt-rotor-tricopter-systems-currently-under-review",
           title: 'Prepared a research manuscript on passive tilt-rotor tricopter systems, currently under review',
           description: "",
-          section: "News",},{id: "projects-project-2",
+          section: "News",},{id: "projects-overactuated-tricopter-uav",
+          title: 'Overactuated Tricopter UAV',
+          description: "This project presents a novel over-actuated tricopter platform.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
