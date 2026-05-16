504f2eca04628704cc1a158aecd180821bddd286
scoczy <151121329+scoczy@users.noreply.github.com>
2026-05-16 07:32:25 +0000

    Deploying to gh-pages from @ scoczy/scoczy.github.io@b70999651e1a5ca29a619299a2369a59a6c856c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,11 +425,6 @@ ninja.data = [{
           description: "This project presents a novel over-actuated tricopter platform.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
